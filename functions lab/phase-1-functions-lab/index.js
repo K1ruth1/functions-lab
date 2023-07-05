294ca2c05f9dda0d5469a5d0a9ef4df52d1b3ce6
K1ruth1 <135802282+K1ruth1@users.noreply.github.com>
2023-07-05 11:56:07 +0300

    Add files via upload

---
+++ functions lab/phase-1-functions-lab/index.js
@@ -0,0 +1,26 @@
+// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+  }
+  
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
